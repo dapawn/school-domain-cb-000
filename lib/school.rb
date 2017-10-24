3e9e6070df278c2346b3e4b6a5eb49ebac3cd415
David Kuruvilla <dapawn@gmail.com>
2017-10-24 22:39:06 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,16 @@
 # code here!
+class School(name)
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize
+    @name = name
+    @roster = {}
+    1..12.each {|grade| roster[grade] = []}
+  end
+
+  def add_student(name,grade)
+    @roster[grade] << name
+  end
+  
+end
